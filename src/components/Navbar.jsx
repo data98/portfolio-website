803cd271f8dd4e37dc03f72fbef87ff90df8918c
data98 <davitgoderdzishvili1998@gmail.com>
2026-02-05 01:13:25 +0400

    initiated navbar components and defining intitial structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { navLinks } from "#constants";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Davit's Portfolio</p>
+
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
