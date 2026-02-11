f32f55a99e17ad0528c0551df1d1c8457ea5c171
data98 <davitgoderdzishvili1998@gmail.com>
2026-02-11 17:10:07 +0400

    added image viewer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap"
 import { Draggable } from "gsap/Draggable"
 
 import { Navbar, Welcome, Dock } from "#components"
-import { Terminal, Safari, Resume, Finder, Text } from "#windows"
+import { Terminal, Safari, Resume, Finder, Text, Image } from "#windows"
 
 gsap.registerPlugin(Draggable)
 
@@ -18,6 +18,7 @@ const App = () => {
       <Resume />
       <Finder />
       <Text />
+      <Image />
     </main>
   )
 }
